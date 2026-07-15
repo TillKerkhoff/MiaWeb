e97c74693044d2875eb9ac13ec9e58db6404559c
Mia <TillKerkhoff@gmail.com>
2026-07-15 11:09:22 +0200

    Refactor home component to use external template and styles

--- angular-MiaWeb/src/app/components/home/home.component.ts
+++ angular-MiaWeb/src/app/components/home/home.component.ts
@@ -3,16 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `
-    <div class="home-container">
-      <h1>Home</h1>
-      <p>Willkommen auf meiner Homepage!</p>
-    </div>
-  `,
-  styles: [`
-    .home-container {
-      padding: 2rem;
-    }
-  `]
+  template: './home.component.html',
+  styles:['./home.component.scss']
 })
 export class HomeComponent {}
